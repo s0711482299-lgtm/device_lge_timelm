474b31a64ea65d45dad1b777cc5ee870e6895116
s0711482299-lgtm <s0711482299@gmail.com>
2026-09-11 06:10:16 -0700

    Update and rename lineage_timelm.mk to halcyon_timelm.mk

--- halcyon_timelm.mk
+++ halcyon_timelm.mk
@@ -11,15 +11,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/lge/timelm/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/halcyon/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_timelm
+PRODUCT_NAME := halcyon_timelm
 PRODUCT_DEVICE := timelm
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LM-V600
 
+# Halcyon flags
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
+
+# GMS
+WITH_GAPPS := false
 PRODUCT_GMS_CLIENTID_BASE := android-lge
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
